29fafee52c4488d855a0e9db97b217418dee2ae7
David Tsai <David@whataniceguy.com>
2015-11-12 11:40:03 -0800

    Completing very basic Angular scaffolding of front end

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,17 @@
-// Please make sure to perform dependency injections here!
+// Dependency injections should happen here!
+// TODO: Please add 'tablesurfer.services' once services have been created
 
 angular.module('tablesurfer', [
-  'ui.router'
+  'ui.router',
+  'ngMaterial'
 ])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('home');
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home/home.html'
+      // controller: 'HomeController' FIXME: Need a home controller
+    })
+})
