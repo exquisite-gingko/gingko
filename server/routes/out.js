43720031723f29de561fb1d37f2925e6ac746b46
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 13:56:04 -0800

    Adding routes to direct requests to the server to the database query file

---
+++ server/routes/out.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var request = require('request');
+
+
+module.exports = function(db) {  
+
+  router.get('/login', function(req, res) {
+    //query the facebook api
+  });
+
+  router.get('/restaurants', function(req, res) {
+    //query an api for restaurant info
+  });
+
+  router.get('/', function(req, res) {
+    //query google for google maps stuff
+  });
+
+};
