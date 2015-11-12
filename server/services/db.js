d81f5b5341a1c84233a1ae8b9c1b2b2c495c2817
Hack Reactor Students <accounts@hackreactor.com>
2015-11-11 20:39:58 -0800

    Adding sequelize tables

---
+++ server/services/db.js
@@ -0,0 +1,69 @@
+var Sequelize = require("sequelize");
+
+//Unsure if we need password, come back to this
+var db = new Sequelize("TableSurf", "root", "");
+
+var Users = db.define("Users", {
+  //here we will have to figure out the data from facebook on authentication
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  photo: {
+    //lookup datatype for sequelize
+  }
+});
+
+var Meals = db.define("Meals", {
+  date: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  time: {
+    //.date is used for timestamp
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  attendees: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+//create user foreign key for meal
+Users.belongsTo(Meals);
+
+var Restaurants = db.define("Restaurants", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  contact: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+  //Do we want to add a city foreign key or zip code?
+});
+
+//create restaurant foreign key for meal
+Restaurants.belongsTo(Meals);
+
+var Genre = db.define("Genre", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+Genre.belongsTo(Meals);
