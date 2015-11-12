947d8a5aae008b146e276744c0088a5dd7c5f2e3
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 14:48:51 -0800

    Writing first tests for user database

--- server/server-spec/server-specs.js
+++ server/server-spec/server-specs.js
@@ -13,7 +13,27 @@ describe("Meal Database", function() {
       //ensure that result gives us back expected post, like meal description
     request({method: "POST", uri: "http://127.0.0.1:4568/api/users", json: {name: "Anna"}});
     request({method: "POST", uri: "http://127.0.0.1:4568/api/meals", json: {date: new Date(), time: new Date(), attendees: 5, description: "new meal", user: "Anna"}}, function);
-    
+
+    //
+
+    //expect(results.length).to.equal(1);
+  });
+
+});
+
+describe("User Database", function() {
+
+  it("Should insert new user to database", function(done) {
+    // Post the user to the chat server.
+    //make post request, inserting new meal
+    //query database
+      //ensure that result gives us back expected post, like meal description
+    request({method: "POST", uri: "http://127.0.0.1:4568/api/users", json: {name: "Anna"}});
+    request({method: "POST", uri: "http://127.0.0.1:4568/api/meals", json: {date: new Date(), time: new Date(), attendees: 5, description: "new meal", user: "Anna"}}, function);
+
+    //
+
+    //expect(results.length).to.equal(1);
   });
 
 });
