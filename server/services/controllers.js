d61ab370570536e40570d3f763ca250303ae6ec4
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 12:17:43 -0800

    Building database controller

--- server/services/controllers.js
+++ server/services/controllers.js
@@ -1,1 +1,14 @@
 var database = require('./db');
+
+module.exports = {
+  users: {
+    get: {},
+    post: {}
+  },
+  meals: {
+    get: {},
+    post: {}
+  },
+  restaurants: {},
+  genre: {}
+}
