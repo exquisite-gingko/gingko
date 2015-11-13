d6d2d16480918048cbbd5b70133426796a95c08e
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-13 12:23:59 -0800

    added passport files incl dependencies

---
+++ server/services/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'facebookAuth': {
+    'clientID': '551360738364791',
+    'clientSecret': '9b1af5a6a734f5d6b2b2f0652959055f',
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  }
+};
