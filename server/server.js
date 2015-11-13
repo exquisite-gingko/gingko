cc08210db617acc10b72a8e7ca38c4bf7b57c3e5
David Tsai <David@whataniceguy.com>
2015-11-12 17:14:11 -0800

    installing nodemon as a dev dependency, running npm start will now run server for us

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 var app = require('./server-config.js');
 
-var port = process.env.PORT || 4568;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
-
