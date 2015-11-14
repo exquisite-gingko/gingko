3f1aadd782a353f31c96765b9a71cb887b51a21f
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-13 16:30:47 -0800

    moved keys outside of git covered files

--- server/services/auth-temp.js
+++ server/services/auth-temp.js
@@ -8,5 +8,13 @@ module.exports = {
     'clientID': 'OUR_CLIENT_ID',
     'clientSecret': 'OUR_CLIENT_SECRET',
     'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  },
+  oauth: {
+    consumer_key: 'OUR_OAUTH_CONSUMER_KEY',
+    token: 'OUR_OAUTH_TOKEN'
+  },
+  yelp: {
+    consumerSecret: 'OUR_YELP_CONSUMER_SECRET',
+    tokenSecret: 'OUR_YELP_TOKEN_SECRET'
   }
 };
