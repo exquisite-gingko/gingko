f279f6087a14685372281a22a8b5dee8912ac3e8
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-13 16:07:59 -0800

    removed fb auth

--- server/services/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'facebookAuth': {
-    'clientID': '551360738364791',
-    'clientSecret': '9b1af5a6a734f5d6b2b2f0652959055f',
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
-  }
-};
