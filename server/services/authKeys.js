76e2b45e037f2a2ce3a5b3a88b6b49d199a76f48
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-16 10:24:24 -0800

    refactoring auth so it works on heroku

---
+++ server/services/authKeys.js
@@ -0,0 +1,15 @@
+module.exports = {
+  facebookAuth: {
+    'clientID': '551360738364791',
+    'clientSecret': '9b1af5a6a734f5d6b2b2f0652959055f',
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  },
+  oauth: {
+    consumer_key: 'TAPaLdRaLTExD0vJ18UMtA',
+    token: 'E9oilk1Ig4L8CTgT_t5juFVNihxQk9S5'
+  },
+  yelp: {
+    consumerSecret: 'Msd-D6cB8c33A-o_ahKYgU-kGHc',
+    tokenSecret: 'CXnmG_Zf_FjHpqAhPFvRIW0ppwE'
+  }
+};
