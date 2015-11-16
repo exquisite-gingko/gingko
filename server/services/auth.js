dabb5c14f06a19e1621d4859fbb4d6471e1536f4
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-16 10:48:52 -0800

    changing auth to not err on heroku

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,10 +1,7 @@
-var authKeys = require('./authKeys');
-
-
 if(process.env.facebookAuth) {
   module.exports.facebookAuth = process.env.facebookAuth;
   module.exports.oauth = process.env.oauth;
   module.exports.yelp = process.env.yelp;
 } else {
-  module.exports = authKeys;
+  module.exports = require('./authKeys');
 };
