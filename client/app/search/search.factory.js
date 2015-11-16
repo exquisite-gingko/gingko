cd957cae74fab477c08ba2c909f50a3e6f9a7eb5
Aaron Ackerman <a.ackerman050@gmail.com>
2015-11-16 11:07:57 -0800

    Building out factory to submit user details

--- client/app/search/search.factory.js
+++ client/app/search/search.factory.js
@@ -1,1 +1,38 @@
 // Placeholder for any sort of search factory
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .factory('homeFactory', homeFactory);
+
+  homeFactory.$inject = ['$http'];
+
+  function homeFactory($http) {
+    var services = {
+
+      activate : activate,
+      postMeal : postMeal
+
+    };
+
+    return services;
+
+    function activate () {
+      postMeal();
+    }
+
+    function postMeal (data) {
+      return $http({
+        method: 'POST',
+        url: '/api/in/meals',
+        data: data
+      })
+      .then(function (response) {
+        console.log('response returned!!');
+        return response.data;
+      });
+    }
+  }
+
+
+})();
